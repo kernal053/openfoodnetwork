ef6702384bed3495391901049840c3222dbc8cb6
kernal053 <uwankar8@gmail.com>
2024-08-16 15:16:44 +0530

    Fix new cell's colspan on cloning

--- app/webpacker/controllers/column_preferences_controller.js
+++ app/webpacker/controllers/column_preferences_controller.js
@@ -3,10 +3,13 @@ import { Controller } from "stimulus";
 // Manage column visibility according to checkbox selection
 //
 export default class ColumnPreferencesController extends Controller {
+  recordedCells = {}
+
   connect() {
     this.table = document.querySelector('table[data-column-preferences-target="table"]');
     this.cols = Array.from(this.table.querySelectorAll('col'));
-    this.colSpanCells = this.table.querySelectorAll('th[colspan],td[colspan]');
+    this.colSpanCells = Array.from(this.table.querySelectorAll('th[colspan],td[colspan]'));
+    this.recordedCells = [...this.colSpanCells];
     // Initialise data-default-col-span
     this.colSpanCells.forEach((cell)=> {
       cell.dataset.defaultColSpan ||= cell.colSpan;
@@ -21,6 +24,26 @@ export default class ColumnPreferencesController extends Controller {
     }
   }
 
+  registerCell() {
+      const registeredElements = this.recordedCells;
+      const newCells = this.colSpanCells.filter(
+        (cell) => !registeredElements.includes(cell)
+      );
+
+      console.log("Identified new cells:", newCells);
+
+      if (newCells.length > 0) {
+        newCells.forEach(cell => {
+          cell.dataset.defaultColSpan ||= cell.colSpan;
+          const hiddenColCount = this.checkboxes.filter((checkbox) => !checkbox.checked).length;
+          const span = parseInt(cell.dataset.defaultColSpan, 10) - hiddenColCount;
+          cell.colSpan = span;
+          this.recordedCells.push(cell);
+        });
+        console.log("Updated recordedCells:", this.recordedCells);
+      }
+  }
+
   // private
 
   #showHideColumn(e) {
@@ -31,7 +54,7 @@ export default class ColumnPreferencesController extends Controller {
 
     // Reset cell colspans
     const hiddenColCount = this.checkboxes.filter((checkbox)=> !checkbox.checked).length;
-    for(const cell of this.colSpanCells) {
+    for(const cell of this.recordedCells) {
       const span = parseInt(cell.dataset.defaultColSpan, 10) - hiddenColCount;
       cell.colSpan = span;
     };
